b8418c2bbe390020a602e1ee8d9356e6a07aeeef
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-06-06 19:31:02 -0500

    added to server file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const path = require("path");
 const express = require('express');
 const session = require('express-session');
+const exphbs = require("express-handlebars");
 const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
@@ -20,8 +23,20 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({
+    helpers: {
+      format_date: date => {
+        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+      }
+    }
+  });
+
+  app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
